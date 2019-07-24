de2fc40bf5fe465db50a3f36496a3c5838bc7ab8
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-24 17:31:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
  
